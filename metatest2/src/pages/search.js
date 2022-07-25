bd68a817389600664111da308a77ef9e744aa363
Alex Breadman <chainetix@gmail.com>
2022-07-25 18:29:26 +0100

    add app

--- metatest2/src/pages/search.js
+++ metatest2/src/pages/search.js
@@ -5,12 +5,15 @@ import TopBar from '../components/top_bar';
 // Step 2: Define your component
 const SearchPage = ({ data }) => {
   const [value, setValue] = React.useState('');
+  const [canRender, setCanRender] = useState(false);
   return (
     <main>
       <style>
       @import url('https://fonts.googleapis.com/css2?family=Cabin:ital,wght@1,600&display=swap');
       </style>
-      <TopBar></TopBar>
+      {
+        canRender ? <TopBar></TopBar>: null
+      }
       
     </main>
   )
